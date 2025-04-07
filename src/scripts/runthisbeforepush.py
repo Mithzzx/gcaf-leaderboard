78546d564bfba73f4a7c1866a28b95f0efbcbadf
Kabillan T A <kabillan1905@gmail.com>
2025-04-08 02:31:10 +0530

    feat: enhanced data security

---
+++ src/scripts/runthisbeforepush.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("public\data.csv")
+
+columns_to_keep = [
+    "Name",
+    "# of Arcade Games Completed",
+    "# of Trivia Games Completed",
+    "# of Skill Badges Completed",
+    "# of Lab-free Courses Completed",
+]
+
+df_cleaned = df[columns_to_keep]
+
+df_cleaned.to_csv("public\data.csv", index=False)
+
+print("Cleaned CSV saved as 'data.csv'")
